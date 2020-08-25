ae033f434b15a5af85a9082fad67d03d6ecc6997
Nathan Barsoti <nathanbarsoti@hotmail.com>
2020-08-25 09:32:29 -0300

    schedules of today and tomorrow

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -2,6 +2,7 @@ import { CustomerBirthday } from './models/birthdays.model';
 import { APPLICATION_API } from './../app.api';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { SchedulesByDay } from './models/schedules.model';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,17 @@ export class DashboardService {
     return new Promise<CustomerBirthday>((resolve, reject) => {
       this.httpClient.get(`${APPLICATION_API}/dashboard/birthdays`)
         .subscribe((response: CustomerBirthday) => {
+          resolve(response);
+        }, reject);
+    }).catch(error => {
+      return Promise.reject(error);
+    });
+  }
+
+  getSchedulesByDay(day: string): Promise<Array<SchedulesByDay>> {
+    return new Promise<Array<SchedulesByDay>>((resolve, reject) => {
+      this.httpClient.get(`${APPLICATION_API}/dashboard/schedules?day=${day}`)
+        .subscribe((response: Array<SchedulesByDay>) => {
           resolve(response);
         }, reject);
     }).catch(error => {
---
+++ src/app/dashboard/models/schedules.model.ts
@@ -0,0 +1,7 @@
+export interface SchedulesByDay {
+    date: any;
+    timeTable: string;
+    Customer: {
+        name: string;
+    }
+}
