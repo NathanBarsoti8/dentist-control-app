3a8f92a92786dd89f939406c36a6af962725f4bb
Nathan Barsoti <nathanbarsoti@hotmail.com>
2020-09-13 09:36:45 -0300

    remove a unused method

--- src/app/shared/services/dateConverter.service.ts
+++ src/app/shared/services/dateConverter.service.ts
@@ -8,17 +8,8 @@ export class DateConverterService {
 
     constructor() { }
 
-    convertStringToDate(date: string): Date {
-        let day = date.slice(0, 2)
-        let month = date.slice(2, 4)
-        let year = date.slice(4, 8)
-
-        return new Date(`${year}-${month}-${day}`);
-    }
-
-    formatStringManually(date: string): string {
-        date = date.replace('/', '');
-        date = date.replace('/', '');
+    convertStringToDate(date: string): string {
+        date = date.replace('/', '').replace('/', '');
 
         let day = date.slice(0, 2)
         let month = date.slice(2, 4)
