6f49e25c849f5ea8352b48e59a48f765cf6f198f
Nathan Barsoti <nathanbarsoti@hotmail.com>
2020-09-28 09:04:41 -0300

    organize paths

--- src/app/attendance/tests/attendance.component.spec.ts
+++ src/app/attendance/tests/attendance.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AttendanceComponent } from './attendance.component';
+import { AttendanceComponent } from '../attendance.component';
 
 describe('AttendanceComponent', () => {
   let component: AttendanceComponent;
