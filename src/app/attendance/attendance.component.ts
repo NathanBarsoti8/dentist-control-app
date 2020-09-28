91bbc584e9fc239df09aed86fde78a0306bb7fc4
Nathan Barsoti <nathanbarsoti@hotmail.com>
2020-09-28 09:05:06 -0300

    component with get all

--- src/app/attendance/attendance.component.ts
+++ src/app/attendance/attendance.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ConfirmDialogData } from 'app/customer/models/confirm-dialog.model';
+import { NotificationService } from 'app/shared/notification/notification.service';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
+import { ServiceType } from './models/attendance.model';
+import { AttendanceService } from './attendance.service';
+import { FormBuilder } from '@angular/forms';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-attendance',
@@ -7,9 +14,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AttendanceComponent implements OnInit {
 
-  constructor() { }
+  // displayedColumns = ['name', 'action'];
+  displayedColumns = ['name'];
+  servicesType: Array<ServiceType>;
+  dialogRef: MatDialogRef<AttendanceComponent>;
+  @ViewChild('createServiceType', { static: true }) createServiceType: TemplateRef<this>;
+
+  constructor(private _attendanceService: AttendanceService,
+    private formBuilder: FormBuilder,
+    // private router: Router,
+    private spinner: NgxSpinnerService,
+    private notification: NotificationService,
+    private dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.getServicesType();
   }
+
+  getServicesType(): void {
+    this.spinner.show();
+    this._attendanceService.getServicesType()
+      .then(result => {
+        if (result) {
+          this.servicesType = result;
+        }
+        else {
+          this.servicesType = undefined;
+        }
+        this.spinner.hide();
+      })
+      .catch(() => {
+        this.spinner.hide();
+        this.notification.showNotification('danger', 'Ocorreu um erro ao carregar os tipos de serviço.', 'error');
+      });
+  }
+
+  // confirmDialog(serviceId: string): void {
+  //   this.deleteDialogRef = this.matDialog.open(SchedulingDeleteComponent, {
+  //     width: '500px',
+  //     height: '210px',
+  //     data: {
+  //       id: serviceId
+  //     }
+  //   });
+
+  //   this.deleteDialogRef.afterClosed()
+  //     .subscribe((response: ConfirmDialogData) => {
+  //       this.onDeleteModalClosed(response);
+  //     });
+  // }
+
+  // onDeleteModalClosed(response: ConfirmDialogData): void {
+  //   if (response.isConfirmed)
+  //     this.deleteSchedule(response.id);
+  // }
+
+  // deleteSchedule(id: string): void {
+  //   this.spinner.show();
+  //   this._attendanceService.delete(id)
+  //     .then(() => {
+  //       this.spinner.hide();
+  //       this.notification.showNotification('success', 'Serviço excluída com sucesso.', 'info');
+  //       this.getServicesType();
+  //     })
+  //     .catch(() => {
+  //       this.spinner.hide();
+  //       this.notification.showNotification('danger', 'Ocorreu um erro ao excluir serviço.', 'error')
+  //     });
+  // }
 
 }
