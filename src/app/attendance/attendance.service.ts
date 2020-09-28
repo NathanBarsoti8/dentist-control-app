5bc4ef635d2861db5eb7f95cd49598a623ff283d
Nathan Barsoti <nathanbarsoti@hotmail.com>
2020-09-28 09:03:57 -0300

    create attendance service

--- src/app/attendance/attendance.service.ts
+++ src/app/attendance/attendance.service.ts
@@ -1,9 +1,16 @@
+import { ServiceType } from './models/attendance.model';
+import { APPLICATION_API } from './../app.api';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DefaultInterface } from 'app/shared/models/default-interface.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AttendanceService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  
+
 }
